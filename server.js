94f9368705ce8e4a3bea0144094782f8a6b532d8
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-16 13:05:17 +0100

    controller: Add create method

--- server.js
+++ server.js
@@ -21,10 +21,23 @@ app.get("/",(req,res)=>{
     res.json({ message: "Welcome to this application. "});
 });
 
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() =>{
+        console.log("Connected to the database!");
+    })
+    .catch(err=> {
+        console.log("Cannot connect to the database!" , err);
+        process.exit();
+    });
+
 
 //set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen( PORT, ()=>{
     console.log(`Server is running on port ${PORT}.`);
 })
-
